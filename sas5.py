528ae15b44e23f9b5a10e21491155c4e9f4cd716
jon373 <josi.ladislao.up@phinmaed.com>
2023-07-07 13:03:51 +0900

    second commit

---
+++ sas5.py
@@ -0,0 +1,4 @@
+import sas55
+lastname = str(input("enter Lastname: "))
+location = str(input("enter your location: "))
+print("your lastname is %s and your location is %s" % (lastname, location), sas55.disp_time())
