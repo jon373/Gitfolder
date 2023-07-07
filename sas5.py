9f086f9467b3a47e596a56daa40c1d7c2c401501
jon373 <josi.ladislao.up@phinmaed.com>
2023-07-07 13:14:56 +0900

    revised

--- sas5.py
+++ sas5.py
@@ -1,4 +1,4 @@
 import sas55
-lastname = str(input("enter Lastname: "))
+lastname = str(input("enter Lastname: TESTING!"))
 location = str(input("enter your location: "))
 print("your lastname is %s and your location is %s" % (lastname, location), sas55.disp_time())
