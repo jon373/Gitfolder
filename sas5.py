cb71a6f58540bab9e927a2bb4f4e3f4cd2b946d7
jon373 <josi.ladislao.up@phinmaed.com>
2023-07-07 22:20:56 +0900

    Binago sample

--- sas5.py
+++ sas5.py
@@ -1,4 +1,4 @@
 import sas55
-lastname = str(input("enter Lastname: TESTING!"))
-location = str(input("enter your location: "))
+lastname = str(input("enter Lastname: Andrei Pogi!"))
+location = str(input("enter your location: Unaek de tapuac"))
 print("your lastname is %s and your location is %s" % (lastname, location), sas55.disp_time())
